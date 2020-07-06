99dd0961aad07343b28a13e82e422c7c57594aa6
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2020-07-06 23:07:19 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default class App extends React.Component {
+
+    /**
+     * Renders the component
+     *
+     * @return {React.Component} The component
+     */
+    render () {
+        return (
+            <div>
+                <h1>Album!</h1>
+            </div>
+        );
+    }
+
+}
