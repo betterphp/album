a1c2a48c6b73919084f5d323f74c0a7ce566e41f
Jacek Kuzemczak <jacek@kuzemcak.co.uk>
2020-07-26 21:53:48 +0100

    Added placeholder file list to dashboard page

--- src/render/pages/dashboard-page.js
+++ src/render/pages/dashboard-page.js
@@ -1,9 +1,71 @@
 import React from "react";
+import { withConfigContext } from "render/context/config-context";
 
-export default class DashboardPage extends React.Component {
+const fs = window.require("electron").remote.require("fs");
 
+class DashboardPage extends React.Component {
+
+    /**
+     * Creates a new instance of the dashboard page
+     *
+     * @param {object} props Input data
+     */
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            files: [],
+        };
+    }
+
+    /**
+     * Called just after the component is added to the DOM
+     *
+     * @return {void}
+     */
+    componentDidMount () {
+        this.updateImageFileList();
+    }
+
+    /**
+     * Fetches a list of image files from the managed folders
+     *
+     * @return {void}
+     */
+    updateImageFileList = () => {
+        this.setState({ files: [] }, () => {
+            this.props.configContext.managedPaths.map((folder) => {
+                fs.readdir(folder, {}, (err, result) => {
+                    const paths = result.map((name) => `${folder}/${name}`);
+
+                    this.setState((prevState) => ({
+                        files: [
+                            ...prevState.files,
+                            ...paths,
+                        ],
+                    }));
+                });
+            });
+        });
+    }
+
+    /**
+     * Render the contents of the dashboard page
+     *
+     * @return {React.Component}
+     */
     render () {
-        return <div>Dashboard</div>;
+        return (
+            <ul>
+                {this.state.files.map((file) => (
+                    <li key={file}>
+                        {file}
+                    </li>
+                ))}
+            </ul>
+        );
     }
 
 }
+
+export default withConfigContext(DashboardPage);
